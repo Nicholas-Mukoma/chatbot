7c60d44b0a6f1557b1098b0c8dc009e1ebead894
mukoma <nicholas2541001@gmail.com>
2024-09-10 23:35:06 +0300

    changed header padding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,10 @@ const Header = () => {
             <h1 className='text-sm xl:text-lg font-bold'>FriendAI</h1>
         </div>
         <div className='flex items-center gap-x-3 xl:gap-x-6 ml-auto'>
-            <h3 className='text-[9px] xl:text-lg text-gray-600'>
+            <h3 className='text-[12px] xl:text-lg text-gray-600'>
                 By Nicholas
             </h3>
-            <button className='bg-violet-400 hover:bg-violet-500 text-[6px] xl:text-[16px] rounded-xl xl:px-4 px-2 py-1'>
+            <button className='bg-violet-400 hover:bg-violet-500 text-[12px] xl:text-[16px] rounded-xl xl:px-4 px-2 py-1'>
                 See Other Projects
             </button>
             
